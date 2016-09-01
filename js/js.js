7f873d35abd1e44d72789357fcac711daf84e80e
ArvinZhu <964505072@qq.com>
2016-09-01 21:58:32 +0800

    评价详情页面增加

--- js/js.js
+++ js/js.js
@@ -31,3 +31,41 @@ window.onload=function(){
             $("#index .district").toggleClass("move-left2");
         })
 }
+function comment(){
+    $(".star").on("click",function(event){
+        event=event?event:window.event;
+        var obj=event.srcElement?event.srcElement:event.target;
+        var ul_index=$(this).index(".star");
+        var li_index=$(obj).index(".star li");
+        var start=5*ul_index;
+        if($($(".star li")[start]).hasClass("star-active")){
+            $(obj.parentNode).children().removeClass("star-active").addClass("star-default");
+        }
+        for( var i=start;i<li_index+1;i++){
+            $($(".star li")[i]).removeClass("star-default").addClass("star-active");
+        }
+    })
+    $("ul .right").on("click",function(event){
+        event=event?event:window.event;
+        var obj=event.srcElement?event.srcElement:event.target;
+        var hook_index=$(obj).index();
+        if(hook_index==1){
+            if($(obj).hasClass("active")){
+                $(obj).removeClass("active")
+            }else{
+                $(obj).addClass("active")
+                var _other=$(obj.parentNode).find("span");
+                 $(_other[3]).removeClass("active");
+            }
+        }
+        if(hook_index==3){
+            if($(obj).hasClass("active")){
+                $(obj).removeClass("active")
+            }else{
+                $(obj).addClass("active")
+                var _other=$(obj.parentNode).find("span");
+                $(_other[1]).removeClass("active");
+            }
+        }
+    })
+}
